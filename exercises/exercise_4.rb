5733cc426d539254284a1210222f3af462fd770e
Youssef Ragab <ragab.youssef95@gmail.com>
2022-03-08 10:32:23 -0500

    exc 4 completed

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,27 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+store_surrey = Store.new(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+store_surrey.save
+store_whistler = Store.new(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+store_whistler.save
+store_yaletown = Store.new(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+store_yaletown.save
+
+# users = User.where(name: 'David', occupation: 'Code Artist').order(created_at: :desc)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+for store in @mens_stores
+  puts store.name
+  puts store.annual_revenue
+end
+
+
+
+@womens_stores = Store.where('annual_revenue < 1000000', womens_apparel: true)
+
+for store in @womens_stores
+  puts store.name
+  puts store.annual_revenue
+end
